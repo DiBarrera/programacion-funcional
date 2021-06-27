b3d5d39fea7ff3d12d58ac72c18c6f6c42732050
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-27 15:55:52 -0500

    Collectors

--- modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
+++ modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -1,13 +1,17 @@
 package com.platzi.functional._15_streams_intro;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TypeStream {
     public static void main(String[] args) {
         IntStream infiniteStream = IntStream.iterate(0, x -> x + 1);
-        infiniteStream.limit(1000)
-                .parallel()
+        List<Integer> numberList = infiniteStream.limit(1000)
+                 // .parallel()
                 .filter(x -> x % 2 == 0)
-                .forEach(System.out::println);
+                .boxed()
+                // .forEach(System.out::println);
+                .collect(Collectors.toList());
     }
 }
