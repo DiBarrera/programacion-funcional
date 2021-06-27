1a36d3b4d69c9b07787280f01fa84c4be995bbc2
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-27 03:11:08 -0500

    Predicate

--- modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
@@ -1,6 +1,7 @@
 package com.platzi.functional._04_functional;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunctions {
     public static void main(String[] args) {
@@ -13,9 +14,32 @@ public class MathFunctions {
         };
         System.out.println(squareFunction.apply(5));
         System.out.println(squareFunction.apply(25));
+
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+
+        isEven.test(4); // is True
+
+        Predicate<Student> isApproved = student -> student.getCalificacion() >= 6.0;
+
+        Student sinuhe = new Student(5.9);
+        System.out.println(isApproved.test(sinuhe));
     }
 
     static int square(int x) {
         return x * x;
+    }
+
+    static class Student {
+        private double calificacion;
+
+        public Student(double calificacion) {
+            this.calificacion = calificacion;
+        }
+
+        public double getCalificacion() {
+            return calificacion;
+        }
     }
 }
